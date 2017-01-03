3102ab8ff9ddeada7adeeab43b9b022a45ae5ddb
zhutc <1097980407@qq.com>
2017-01-04 05:46:32 +0800

    modify select course

--- application/models/MainModel.class.php
+++ application/models/MainModel.class.php
@@ -3,22 +3,22 @@
 class MainModel extends Model {
 
     public function selectonebyitem($item, $uid) {
-        $sql = "select $item from $this->table where uid = '$uid'";
+        $sql = "select $item from $this->table where uid = $uid";
         return $this->db->getOne($sql);
     }
 
     public function selectallbyitem($identify) {
-        $sql = "select uid, uname from $this->table where identify = $identify";
+        $sql = "select uid, uname, introduction from $this->table, profile where identify = $identify and profile.uid = $this->table.uid";
         return $this->db->getAll($sql);
     }
 
     public function selectallbycourse($cid) {
-        $sql = "select uid, uname from $this->table, user where cid = $cid and user.uid = $this->table.uid";
+        $sql = "select user.uid, uname, introduction from $this->table, user, profile where cid = $cid and user.uid = $this->table.uid and profile.uid = user.uid";
         return $this->db->getAll($sql);
     }
 
     public function selectallbyuid($uid) {
-        $sql = "select cid, cname from $this->table, course where uid = $uid and course.cid = $this->table.cid";
+        $sql = "select course.cid, cname, introduction from $this->table, course, profile where uid = $uid and course.cid = $this->table.cid and profile.uid = $uid";
         return $this->db->getAll($sql);
     }
 
--- function.sql
+++ function.sql
@@ -1,3 +1,5 @@
+
+-- 计算注册总人数
 drop function if exists calNumberofRegistered;
 DELIMITER //
 create function calNumberofRegistered()
@@ -12,6 +14,8 @@ DELIMITER ;
 
 select calNumberofRegistered();
 
+
+-- 计算学生总人数
 drop function if exists calNumberofStudent;
 DELIMITER //
 create function calNumberofStudent()
@@ -26,6 +30,8 @@ DELIMITER ;
 
 select calNumberofStudent();
 
+
+-- 计算老师总人数
 drop function if exists calNumberofTeacher;
 DELIMITER //
 create function calNumberofTeacher()
@@ -39,3 +45,111 @@ create function calNumberofTeacher()
 DELIMITER ;
 
 select calNumberofTeacher();
+
+
+-- 计算学生总选课个数
+drop function if exists sumofStudentchooseCourse;
+DELIMITER //
+create function sumofStudentchooseCourse(studentid int)
+    returns int
+    begin
+    declare sum int;
+    select count(*) into sum from selectcourse where uid_student = studentid;
+    return sum;
+    end;
+//
+DELIMITER ;
+
+select sumofStudentchooseCourse(1);
+
+
+-- 计算老师总代课个数
+drop function if exists sumofTeacherteachCourse;
+DELIMITER //
+create function sumofTeacherteachCourse(teacherid int)
+    returns int
+    begin
+    declare sum int;
+    select count(*) into sum from selectcourse where uid_teacher = teacherid;
+    return sum;
+    end;
+//
+DELIMITER ;
+
+select sumofTeacherteachCourse(1);
+
+
+-- 计算学生平均每人选课个数
+drop function if exists avgofStudentchooseCourse;
+DELIMITER //
+create function avgofStudentchooseCourse()
+    returns double precision
+    begin
+    declare sumselect int;
+    declare sumstudent int;
+    declare avg double precision;
+    select count(*) into sumselect from selectcourse;
+    select calNumberofStudent() into sumstudent;
+    return sumselect/sumstudent;
+    end;
+//
+DELIMITER ;
+
+select avgofStudentchooseCourse();
+
+
+-- 计算老师平均每人代课个数
+drop function if exists avgofTeacherteachCourse;
+DELIMITER //
+create function avgofTeacherteachCourse()
+    returns double precision
+    begin
+    declare sumteach int;
+    declare sumteacher int;
+    declare avg double precision;
+    select count(*) into sumteach from selectcourse;
+    select calNumberofTeacher() into sumteacher;
+    return sumteach/sumteacher;
+    end;
+//
+DELIMITER ;
+
+select avgofTeacherteachCourse();
+
+
+-- 计算选过课的学生占总学生数量的百分比
+drop function if exists studentSelectCoursePercent;
+DELIMITER //
+create function studentSelectCoursePercent()
+    returns double precision
+    begin
+    declare selectcoursestudentnum int;
+    declare allstudentnum int;
+    declare percent double precision;
+    select count(uid_student) into selectcoursestudentnum from selectcourse;
+    select calNumberofStudent() into allstudentnum;
+    return selectcoursestudentnum/allstudentnum;
+    end;
+//
+DELIMITER ;
+
+select studentSelectCoursePercent();
+
+
+-- 计算代过课的老师占总老师数量的百分比
+drop function if exists teacherTeachCoursePercent;
+DELIMITER //
+create function teacherTeachCoursePercent()
+    returns double precision
+    begin
+    declare teachcourseteachernum int;
+    declare allteachernum int;
+    declare percent double precision;
+    select count(uid_teacher) into teachcourseteachernum from selectcourse;
+    select calNumberofTeacher() into allteachernum;
+    return teachcourseteachernum/allteachernum;
+    end;
+//
+DELIMITER ;
+
+select teacherTeachCoursePercent();
