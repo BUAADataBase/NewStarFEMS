b21fb9abf422e12c2b581c67a45d8c1850a9e4c4
EmperorKang <839248926@qq.com>
2017-01-04 06:32:39 +0800

    choose_course

--- application/views/home/choose_course.php
+++ application/views/home/choose_course.php
@@ -37,23 +37,92 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
             });
 </script>
 <script type="text/javascript">
+course="";
+pages=0;
 getname_success=function(data){
     var user_name=document.getElementById("welcome_user").innerHTML="<font color=\"white\">欢迎，"+data.name+"</font>";
 }
-getteachers_success=function(data){
+getteachersbycourse_success=function(data){
+    pages=parseInt(data.length/6)+1;
+    var teacherslist=data.teacherlist;
+    var teacherlegth=(teacherslist.length>6)?6:teacherslist.length;
+    if(pages<5){
+        for(var i=5;i!=pages;i--){
+            var str="a"+i;
+            document.getElementById(str).parentNode.removeChild(document.getElementById(str));
+        }
+    }
+    for(var i=0;i<teacherlegth;i++){
+        var temp=i+1;
+        var str_course="course"+temp;
+        var str_teacher="teacher"+temp;
+        var str_profile="profile"+temp;
+        document.getElementById(str_course).innerHTML=course;
+        document.getElementById(str_teacher).innerHTML=teacherslist[i].uname;
+        document.getElementById(str_teacher).innerHTML=teacherslist[i].profile;
+    }
 
 }
     function get_teachers(){
         var url="http://localhost:8080/index.php?c=Main&a=getname";
         ajax_send(url,0,getname_success,load_error);
-        url="http://localhost:8080/index.php?c=Main&a=getAllTeacherList";
-        ajax_send(url,0,gettachers_success,load_error);
+        var user_search=document.getElementById("search");
+        var course_number;
+        var flag;
+        if(user_search.value==""){
+            alert("查询不能为空！");
+            flag=2;
+        }
+        else if(user_search.value=="语文"){
+            course_number=2;
+            course="语文";
+            flag=1;
+        }
+        else if(user_search.value=="数学"){
+            course_number=1;
+            course="数学";
+            flag=1;
+        }
+        else if(user_search.value=="英语"){
+            course_number=3;
+            course="英语";
+            flag=1;
+        }
+        else if(user_search.value=="物理"){
+            course_number=4;
+            course="物理";
+            flag=1;
+        }
+        else if(user_search.value=="化学"){
+            course="化学";
+            course_number=5;
+            flag=1;
+        }
+        else if(user_search.value=="生物"){
+            course="生物";
+            course_number=6;
+            flag=1;
+        }
+        else if(user_search.value.length>12){
+            alert("搜索内容太长！");
+            flag=2;
+        }
+        else{
+            course_number=user_search.value;
+            flag=0;
+        }
+        if(flag==1){
+            var json="{\"courseid\":"+course_number+"}";
+            url="http://localhost:8080/index.php?c=Main&a=getTeacherListbyCourse"
+            json=JSON.parse(json);
+            ajax_send(url,json,getteachersbycourse_success,load_error);
+        }
         alert("get!");
     }
 </script>
 <!--/script-->
 </head>
-    <body  onload="get_teachers()">
+    <body>
 <!--header-->
         <div class="header" id="home">
             <nav class="navbar navbar-default">
@@ -113,9 +182,9 @@ getteachers_success=function(data){
     <div class="container">
             <div class="col-lg-6 in-gp-tb">
                 <div class="input-group">
-                    <input class="form-control" placeholder="Search for..." type="text" style="position:absolute;left:300px;width:500px;">
+                    <input class="form-control" id="search" placeholder="Search for..." type="text" style="position:absolute;left:300px;width:500px;">
                     <span class="input-group-btn">
-                        <button class="btn btn-default" type="button" style="position:absolute;left:800px;">Go!</button>
+                        <button class="btn btn-default" type="button" style="position:absolute;left:800px;" onclick="get_teachers()">Go!</button>
                     </span>
                 </div>
             </div>
@@ -136,8 +205,9 @@ getteachers_success=function(data){
                     <figure>
                         <img src="http://localhost:8080/application/views/home/images/a1.jpg" alt="img03">
                         <figcaption>
-                        <h3 id="course1">course</h3>
-                            <span id="teacher1">teacher1</span>
+                        <center><h3 id="course1">请选择课程或老师</h3></center>
+                            <span id="teacher1" style="position:absolute;left:140px;top:230px;">空</span>
+                            <pre id="profile1" style="position:absolute;left:20px;top:50px;width:80px;height:200px;white-space: pre-wrap;word-wrap: break-word;">profile:空</pre>
                             <input type="button" id="choose1" style="width: auto; background:#232428;color: #fff;font-size: 0.8725em;padding: 7px 20px;border: none;margin: 0 12px 0 0;cursor: pointer;transition: all .5s;-webkit-transition: all .5s;-moz-transition: all .5s;-o-transition: all .5s;outline:none;text-transform: uppercase;position:absolute;left:250px;top:225px;" value="选课">
                         </figcaption>
                     </figure>
@@ -146,8 +216,9 @@ getteachers_success=function(data){
                     <figure>
                         <img src="http://localhost:8080/application/views/home/images/a2.jpg" alt="img04">
                         <figcaption>
-                        <h3 id="course2">course2</h3>
-                            <span id="teacher2">teacher2</span>
+                        <center><h3 id="course2">请选择课程或老师</h3></center>
+                            <span id="teacher2" style="position:absolute;left:140px;top:230px;">空</span>
+                            <pre id="profile2" style="position:absolute;left:20px;top:50px;width:80px;height:200px;white-space: pre-wrap;word-wrap: break-word;">profile:空</pre>
                             <input type="button" id="choose2" style="width: auto; background:#232428;color: #fff;font-size: 0.8725em;padding: 7px 20px;border: none;margin: 0 12px 0 0;cursor: pointer;transition: all .5s;-webkit-transition: all .5s;-moz-transition: all .5s;-o-transition: all .5s;outline:none;text-transform: uppercase;position:absolute;left:250px;top:225px;" value="选课">
                         </figcaption>
                     </figure>
@@ -156,8 +227,9 @@ getteachers_success=function(data){
                     <figure>
                         <img src="http://localhost:8080/application/views/home/images/a3.jpg" alt="img01">
                         <figcaption>
-                        <h3 id="course3">course3</h3>
-                            <span id="teacher3">teacher3</span>
+                        <center><h3 id="course3">请选择课程或老师</h3></center>
+                            <span id="teacher3" style="position:absolute;left:140px;top:230px;">空</span>
+                            <pre id="profile3" style="position:absolute;left:20px;top:50px;width:80px;height:200px;white-space: pre-wrap;word-wrap: break-word;">profile:空</pre>
                             <input type="button" id="choose3" style="width: auto; background:#232428;color: #fff;font-size: 0.8725em;padding: 7px 20px;border: none;margin: 0 12px 0 0;cursor: pointer;transition: all .5s;-webkit-transition: all .5s;-moz-transition: all .5s;-o-transition: all .5s;outline:none;text-transform: uppercase;position:absolute;left:250px;top:225px;" value="选课">
                         </figcaption>
                     </figure>
@@ -166,8 +238,9 @@ getteachers_success=function(data){
                     <figure>
                         <img src="http://localhost:8080/application/views/home/images/a4.jpg" alt="img02">
                         <figcaption>
-                        <h3 id="course4">cours4</h3>
-                            <span id="teacher1">teacher4</span>
+                        <center><h3 id="course4">请选择课程或老师</h3></center>
+                            <span id="teacher4" style="position:absolute;left:140px;top:230px;">空</span>
+                            <pre id="profile4" style="position:absolute;left:20px;top:50px;width:80px;height:200px;white-space: pre-wrap;word-wrap: break-word;">profile:空</pre>
                             <input type="button" id="choose1" style="width: auto; background:#232428;color: #fff;font-size: 0.8725em;padding: 7px 20px;border: none;margin: 0 12px 0 0;cursor: pointer;transition: all .5s;-webkit-transition: all .5s;-moz-transition: all .5s;-o-transition: all .5s;outline:none;text-transform: uppercase;position:absolute;left:250px;top:225px;" value="选课">
                         </figcaption>
                     </figure>
@@ -176,8 +249,9 @@ getteachers_success=function(data){
                     <figure>
                         <img src="http://localhost:8080/application/views/home/images/a5.jpg" alt="img06">
                         <figcaption>
-                        <h3 id="course5">cours5</h3>
-                            <span id="teacher5">teacher5</span>
+                        <center><h3 id="course5">请选择课程或老师</h3></center>
+                            <span id="teacher5" style="position:absolute;left:140px;top:230px;">空</span>
+                            <pre id="profile5" style="position:absolute;left:20px;top:50px;width:80px;height:200px;white-space: pre-wrap;word-wrap: break-word;">profile:空</pre>
                             <input type="button" id="choose5" style="width: auto; background:#232428;color: #fff;font-size: 0.8725em;padding: 7px 20px;border: none;margin: 0 12px 0 0;cursor: pointer;transition: all .5s;-webkit-transition: all .5s;-moz-transition: all .5s;-o-transition: all .5s;outline:none;text-transform: uppercase;position:absolute;left:250px;top:225px;" value="选课">
                         </figcaption>
                     </figure>
@@ -186,21 +260,34 @@ getteachers_success=function(data){
                     <figure>
                         <img src="http://localhost:8080/application/views/home/images/a6.jpg" alt="img05">
                         <figcaption>
-                        <h3 id="course6">course6</h3>
-                            <span id="teacher6">teacher6</span>
+                        <center><h3 id="course6">请选择课程或老师</h3></center>
+                            <span id="teacher6" style="position:absolute;left:140px;top:230px;">空</span>
+                            <pre id="profile6" style="position:absolute;left:20px;top:50px;width:80px;height:200px;white-space: pre-wrap;word-wrap: break-word;">profile:空</pre>
                             <input type="button" id="choose6" style="width: auto; background:#232428;color: #fff;font-size: 0.8725em;padding: 7px 20px;border: none;margin: 0 12px 0 0;cursor: pointer;transition: all .5s;-webkit-transition: all .5s;-moz-transition: all .5s;-o-transition: all .5s;outline:none;text-transform: uppercase;position:absolute;left:250px;top:225px;" value="选课">
                         </figcaption>
                     </figure>
                 </li>
             </ul>
-            <ul class="pagination pagination-lg">
-                <li><a href="#" onclick="edithtml()">&laquo;</a></li>
-                <li><a href="#">1</a></li>
-                <li><a href="#">2</a></li>
-                <li><a href="#">3</a></li>
-                <li><a href="#">4</a></li>
-                <li><a href="#">5</a></li>
-                <li><a href="#">&raquo;</a></li>
+            <ul class="pagination pagination-lg" id="u1">
+                <li><a href="#" onclick="left_move()">&laquo;</a></li>
+                <script type="text/javascript">
+                function left_move(){
+                    var first_page=document.getElementById("a1");
+                    if(first_page.innerHTML==1){
+                        ;
+                    }
+                    else{
+                        var des_page=first_page.value-1;
+                        var url="http://localhost:8080/index.php?c=Main&";
+                    }
+                }
+                </script>
+                <li id="a1"><a href="#">1</a></li>
+                <li id="a2"><a href="#">2</a></li>
+                <li id="a3"><a href="#">3</a></li>
+                <li id="a4"><a href="#">4</a></li>
+                <li id="a5"><a href="#">5</a></li>
+                <li id="right"><a href="#">&raquo;</a></li>
             </ul>
     </div>
 </div>
